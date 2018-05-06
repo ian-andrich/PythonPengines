fc8579e9eb41906d69f7249ff6ad665693cc54d0
cmungall <cjm@berkeleybop.org>
2018-05-06 11:28:15 -0700

    Suggested changes for test/example. Use an example that has 1000 solutions (see #8) Demonstrate bundling ask with initial request (saves additional roundtrip) only show all solutions at end

--- tests/tests.py
+++ tests/tests.py
@@ -1,11 +1,10 @@
 from pengines.Builder import PengineBuilder
 from pengines.Pengine import Pengine
 
-factory = PengineBuilder(urlserver="http://localhost:4242", destroy=False)
+q = "between(1,1000,X)"
+factory = PengineBuilder(urlserver="http://localhost:4242", destroy=False, ask=q)
 pengine = Pengine(builder=factory, debug=True)
 # Start query.
-pengine.ask("member(X, [1,2,3])")
-pengine.doAsk(pengine.currentQuery)
 print()
 print(pengine.currentQuery.availProofs, "Has More? ", pengine.currentQuery.hasMore)
 print()
@@ -13,4 +12,4 @@ print()
 print(pengine.state.current_state)
 while pengine.currentQuery.hasMore:
     pengine.doNext(pengine.currentQuery)
-    print(pengine.currentQuery.availProofs)
+print(pengine.currentQuery.availProofs)
