0ef405496275a3339fd19fce02b6558f4afbad96
Ian Andrich <iandrich87+git@gmail.com>
2019-08-17 00:05:38 -0700

    Adjusted the project as appropriate to use travis-ci to run our tests.  Adjusted tests to use the correct port in the travis-ci docker file.

--- tests/srctest_test.py
+++ tests/srctest_test.py
@@ -14,10 +14,10 @@ class PenginesTestCase(unittest.TestCase):
 
     def test_member(self):
         """
-        
+
         """
         q = "member(X,[1,2,3])"
-        factory = PengineBuilder(urlserver="http://localhost:9083", destroy=False, ask=q)
+        factory = PengineBuilder(urlserver="http://localhost:4242", destroy=False, ask=q)
         pengine = Pengine(builder=factory, debug=True)
         results = pengine.currentQuery.availProofs
         print(results)
@@ -25,14 +25,14 @@ class PenginesTestCase(unittest.TestCase):
         self.assertTrue( {'X':1} in results )
         self.assertTrue( {'X':2} in results )
         self.assertTrue( {'X':3} in results )
-    
+
     def test_src(self):
         """
-        
+
         """
         src = "foo(a).\nfoo(b).\nfoo(c)."
         q = "foo(X)"
-        factory = PengineBuilder(urlserver="http://localhost:9083", destroy=False, srctext=src, ask=q)
+        factory = PengineBuilder(urlserver="http://localhost:4242", destroy=False, srctext=src, ask=q)
         pengine = Pengine(builder=factory, debug=True)
         results = pengine.currentQuery.availProofs
         print(results)
@@ -40,10 +40,10 @@ class PenginesTestCase(unittest.TestCase):
         self.assertTrue( {'X':'a'} in results )
         self.assertTrue( {'X':'b'} in results )
         self.assertTrue( {'X':'c'} in results )
-    
+
     def test_chunk(self):
         """
-        
+
         """
         src = "foo(a).\nfoo(b).\nfoo(c)."
         q = "foo(X)"
@@ -64,25 +64,25 @@ class PenginesTestCase(unittest.TestCase):
 
     def test_iterator(self):
         """
-        
+
         """
         q = "member(X,[1,2,3,4,5,6,7,8,9,10])"
         chunk_sizes = [1,2,3,4,100]
 
         for chunk in chunk_sizes:
-            factory = PengineBuilder(urlserver="http://localhost:9083", destroy=False, chunk=chunk, ask=q)
+            factory = PengineBuilder(urlserver="http://localhost:4242", destroy=False, chunk=chunk, ask=q)
             pengine = Pengine(builder=factory, debug=True)
             results = []
             for r in pengine.currentQuery:
                 print('ITER={}'.format(r))
                 results.append(r)
-            
+
             self.assertTrue(len(results) == 10)
             self.assertTrue( {'X':1} in results )
             self.assertTrue( {'X':2} in results )
             self.assertTrue( {'X':3} in results )
-    
-        
-    
+
+
+
 if __name__ == '__main__':
     unittest.main()
